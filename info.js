26af648be1495e3a6a2c8d63452922c5900c984c
Manan Sodha <66784003+manansodha@users.noreply.github.com>
2022-08-24 21:08:45 +0530

    Add files via upload

---
+++ info.js
@@ -0,0 +1,171 @@
+function info(){
+
+    const f = 'abcdefghijklmnopqrstuvwxy';
+    const row = document.createElement("tr");
+    const data = document.createElement("td");
+    const card = document.createElement("div");
+    card.className = "club-card";
+    const tumb = document.createElement("div");
+    tumb.className = "card-tumb";
+    const details = document.createElement("div");
+    details.className = "club-details"; 
+    const bottom = document.createElement("div");
+    bottom.className = "club-bottom";
+    const type = document.createElement("span");
+    type.className = 'club-type';
+    const clb_name = document.createElement('h4');
+    const clb_name_link = document.createElement('a');
+    const text = document.createElement("p");
+    const btn = document.createElement('button');
+    btn.id = 'join-btn';
+    const image = document.createElement("img");
+    image.src = "C:/Users/kshro/Documents/Project/vitlogo.png";
+    tumb.appendChild(image);
+
+    type.textContent = "Technical Club";
+    clb_name_link.textContent = "Antrix Club";
+    text.textContent = "abcdefghijklmnopqrstuvwxyz";
+    btn.textContent = "Join";
+
+    
+    data.append(card);
+    card.append(tumb);
+    card.append(details);
+    details.append(type);
+    details.append(clb_name);
+    clb_name.appendChild(clb_name_link);
+    details.append(text);
+    details.append(bottom);
+    bottom.appendChild(btn);
+
+
+    const table = document.querySelector("#tech-club");
+    for (var i = 0; i<=3; i++){
+    
+        var x = data.cloneNode(true);
+        row.append(x);
+    
+    } 
+    for (var i = 0; i<=6; i++){
+    
+        var x = row.cloneNode(true);
+        table.append(x);
+    
+    }
+}
+
+
+function info1(){
+
+    const f = 'abcdefghijklmnopqrstuvwxy';
+    const row = document.createElement("tr");
+    const data = document.createElement("td");
+    const card = document.createElement("div");
+    card.className = "club-card";
+    const tumb = document.createElement("div");
+    tumb.className = "card-tumb";
+    const details = document.createElement("div");
+    details.className = "club-details"; 
+    const bottom = document.createElement("div");
+    bottom.className = "club-bottom";
+    const type = document.createElement("span");
+    type.className = 'club-type';
+    const clb_name = document.createElement('h4');
+    const clb_name_link = document.createElement('a');
+    const text = document.createElement("p");
+    const btn = document.createElement('button');
+    btn.id = 'join-btn';
+    const image = document.createElement("img");
+    image.src = "C:/Users/kshro/Documents/Project/vitlogo.png";
+    tumb.appendChild(image);
+
+    type.textContent = "Non-technical Club";
+    clb_name_link.textContent = "NSS";
+    text.textContent = "abcdefghijklmnopqrstuvwxyz";
+    btn.textContent = "Join";
+
+    
+    data.append(card);
+    card.append(tumb);
+    card.append(details);
+    details.append(type);
+    details.append(clb_name);
+    clb_name.appendChild(clb_name_link);
+    details.append(text);
+    details.append(bottom);
+    bottom.appendChild(btn);
+
+
+    const table = document.querySelector("#tech-club");
+    for (var i = 0; i<=3; i++){
+    
+        var x = data.cloneNode(true);
+        row.append(x);
+    
+    } 
+    for (var i = 0; i<=2; i++){
+    
+        var x = row.cloneNode(true);
+        table.append(x);
+    
+    }
+}
+
+
+
+function info2(){
+
+    const f = 'abcdefghijklmnopqrstuvwxy';
+    const row = document.createElement("tr");
+    const data = document.createElement("td");
+    const card = document.createElement("div");
+    card.className = "club-card";
+    const tumb = document.createElement("div");
+    tumb.className = "card-tumb";
+    const details = document.createElement("div");
+    details.className = "club-details"; 
+    const bottom = document.createElement("div");
+    bottom.className = "club-bottom";
+    const type = document.createElement("span");
+    type.className = 'club-type';
+    const clb_name = document.createElement('h4');
+    const clb_name_link = document.createElement('a');
+    const text = document.createElement("p");
+    const btn = document.createElement('button');
+    btn.id = 'join-btn';
+    const image = document.createElement("img");
+    image.src = "C:/Users/kshro/Documents/Project/vitlogo.png";
+    tumb.appendChild(image);
+
+    type.textContent = "Language Club";
+    clb_name_link.textContent = "Hindi";
+    text.textContent = "abcdefghijklmnopqrstuvwxyz";
+    btn.textContent = "Join";
+
+    
+    data.append(card);
+    card.append(tumb);
+    card.append(details);
+    details.append(type);
+    details.append(clb_name);
+    clb_name.appendChild(clb_name_link);
+    details.append(text);
+    details.append(bottom);
+    bottom.appendChild(btn);
+
+
+    const table = document.querySelector("#tech-club");
+    for (var i = 0; i<=3; i++){
+    
+        var x = data.cloneNode(true);
+        row.append(x);
+    
+    } 
+    for (var i = 0; i<=1; i++){
+    
+        var x = row.cloneNode(true);
+        table.append(x);
+    
+    }
+}
+
